e8ee2448d1ac72b8a32c7a889f49db3a8f390faf
zerrty <pgrytsyshyn@gmail.com>
2023-08-21 17:48:33 +0300

    complete menu burger, add adaptive to header

--- index.js
+++ index.js
@@ -27,13 +27,37 @@ clear.addEventListener('click', (evt) =>{
 
 // add navigation from header to burger-navigation window
 const navigationWrapper = document.querySelectorAll('.header__navigation-list');
-window.addEventListener('resize', (evt) => {
+document.addEventListener("DOMContentLoaded", (evt) =>{
     if(document.documentElement.clientWidth < 769){
-        navigationWrapper[0].classList.remove('activeMenu');
-        navigationWrapper[1].classList.add('activeMenu');
+        navigationWrapper[0].classList.remove('_active');
     } else{
-        navigationWrapper[1].classList.remove('activeMenu');
-        navigationWrapper[0].classList.add('activeMenu');
+        navigationWrapper[1].classList.remove('_active');
+        navigationWrapper[0].classList.add('_active');
+    }
+});
+
+window.addEventListener('resize', (evt) =>{
+    if(document.documentElement.clientWidth < 769){
+        navigationWrapper[0].classList.remove('_active');
+    } else{
+        navigationWrapper[1].classList.remove('_active');
+        navigationWrapper[0].classList.add('_active');
     }
 });
 // console.log(navigationWrapper[0], navigationWrapper[1]);
+
+
+// burger button
+const burgerBtn = document.querySelector('.header__burger');
+if(burgerBtn){
+    const menuBody = document.querySelector('.menu');
+    const milkShadow = document.querySelector('.milk-shadow');
+
+    burgerBtn.addEventListener('click', (evt) =>{
+        burgerBtn.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+        milkShadow.classList.toggle('_active');
+        document.body.classList.toggle('_lock');
+        navigationWrapper[1].classList.toggle('_active');
+    })
+}
